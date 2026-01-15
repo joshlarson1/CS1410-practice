27e24c755961cbde2c7b4e8c4c74590698872b4c
joshlarson1 <joshlarson1@mail.weber.edu>
2026-01-15 19:44:02 +0000

    Finished ex0101

---
+++ src/Example0101.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int x, y;
+cout<< "Enter x: ";
+cin>> x;
+
+cout<<"Enter Y: ";
+cin>> y;
+
+cout<< "x + y =" <<(x+y) << endl;
+cout<< "x - y =" <<(x-y) << endl;
+cout<< "x * y =" <<(x*y) << endl;
+cout<< "x / y =" <<(x/y) << endl;
+
+return 0;
+}
